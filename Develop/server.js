2ea2a9a20d9fd4cf1506aec9d0c9705136e9dd7b
kgarden22 <kgarden22@gmail.com>
2020-06-08 20:53:18 -0600

    went back to boiler plate

--- Develop/server.js
+++ Develop/server.js
@@ -5,17 +5,12 @@
 // *** Dependencies
 // =============================================================
 const express = require("express");
-const fs = require("fs")
-const https = require("https")
-const path = require("path")
+
 // Sets up the Express App
 // =============================================================
 const app = express();
 const PORT = process.env.PORT || 8080;
-const server = https.createServer(app)
-server.listen(PORT, => {
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+
 // Requiring our models for syncing
 const db = require("./models");
 
@@ -28,15 +23,13 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
-// require("./routes/api-routes.js")(app);
-// require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-
-// const server = https.createServer(app)
-// db.sequelize.sync({ force: true }).then(function() {
-//   app.listen(PORT, function() {
-//     console.log("App listening on PORT " + PORT);
-//   });
-// });
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
