048d3f1fb020f8d7ae32c4f7d20da659235d1cb9
jherd801 <jherd801@gmail.com>
2020-06-08 19:44:23 -0600

    Fixed formatting after failed travis CI

--- geolocation.js
+++ geolocation.js
@@ -1,36 +1,36 @@
 function geoFindMe() {
+  const status = document.querySelector("#status");
+  const mapLink = document.querySelector("#map-link");
+  const googleLink = document.querySelector("#google-link");
 
-    const status = document.querySelector('#status');
-    const mapLink = document.querySelector('#map-link');
-    const googleLink = document.querySelector('#google-link');
-  
-    mapLink.href = '';
-    mapLink.textContent = '';
-    googleLink.href = '';
-    googleLink.textContent = '';
-  
-    function success(position) {
-      const latitude  = position.coords.latitude;
-      const longitude = position.coords.longitude;
-  
-      status.textContent = '';
-      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-      googleLink.href = 'https://www.google.com/maps/place/${latitude}/${longitude}'
-      googleLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-    }
-  
-    function error() {
-      status.textContent = 'Unable to retrieve your location';
-    }
-  
-    if(!navigator.geolocation) {
-      status.textContent = 'Geolocation is not supported by your browser';
-    } else {
-      status.textContent = 'Locating…';
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
-  
+  mapLink.href = "";
+  mapLink.textContent = "";
+  googleLink.textContent = "";
+  googleLink.textContent = "";
+
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = "";
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = "Latitude: ${latitude} °, Longitude: ${longitude} °";
+    googleLink.href =
+      "https://www.google.com/maps/place/?api=1&query=40.7175168,-111.8208";
+    googleLink.textContent =
+      "Latitude: ${latitude} °, Longitude: ${longitude} °";
   }
-  
-  document.querySelector('#find-me').addEventListener('click', geoFindMe);
+
+  function error() {
+    status.textContent = "Unable to retrieve your location";
+  }
+
+  if (!navigator.geolocation) {
+    status.textContent = "Geolocation is not supported by your browser";
+  } else {
+    status.textContent = "Locating…";
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
+document.querySelector("#find-me").addEventListener("click", geoFindMe);
